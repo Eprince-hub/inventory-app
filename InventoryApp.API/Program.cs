2f8dd70eafb837bac4723f3005280d3c2e2232e4
Victor Ejike Nwosu <ejikello123@yahoo.com>
2025-06-09 21:15:29 +0200

    Add initial endpoints

--- InventoryApp.API/Program.cs
+++ InventoryApp.API/Program.cs
@@ -1,6 +1,30 @@
+// Similar to express server
+using InventoryApp.Shared;
+using Microsoft.AspNetCore.Http.HttpResults;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Dependencies Injection
+builder.Services.AddSingleton<IProductService>(new ProductService());
+builder.Services.AddScoped<ProductValidationFilter>();
+
+// App Builder
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/products", (IProductService service) => service.GetProducts());
+
+app.MapGet("/products/{id}", Results<Ok<Product>, NotFound> (int id, IProductService service) =>
+{
+  var product = service.GetProduct(id);
+  return product is not null ? TypedResults.Ok(product) : TypedResults.NotFound();
+});
+
+app.MapPost("/products", (Product product, IProductService service) =>
+{
+  var newProduct = product with { Id = service.GetProducts().Count + 1, SKU = Guid.NewGuid().ToString(), IsArchived = false };
+
+  service.AddProduct(newProduct);
+  return TypedResults.Created($"/products/{newProduct.Id}", newProduct);
+}).AddEndpointFilter<ProductValidationFilter>();
 
 app.Run();
