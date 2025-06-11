bc782f8cb475a39d9e677e426dee8f76c8469e4a
Victor Ejike Nwosu <ejikello123@yahoo.com>
2025-06-11 17:54:19 +0200

    Refactor product route mappings into MapProductRoutes method for cleaner Program.cs

--- InventoryApp.API/Program.cs
+++ InventoryApp.API/Program.cs
@@ -1,7 +1,4 @@
 // Similar to express server
-using InventoryApp.Shared;
-using Microsoft.AspNetCore.Http.HttpResults;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Dependencies Injection
@@ -11,30 +8,6 @@ builder.Services.AddScoped<ProductValidationFilter>();
 // App Builder
 var app = builder.Build();
 
-app.MapGet("/products", (IProductService service) => service.GetProducts());
-
-app.MapGet("/products/{id}", Results<Ok<Product>, NotFound> (int id, IProductService service) =>
-{
-  var product = service.GetProduct(id);
-  return product is not null ? TypedResults.Ok(product) : TypedResults.NotFound();
-});
-
-app.MapPost("/products", (Product product, IProductService service) =>
-{
-  var newProduct = product with { Id = service.GetProducts().Count + 1, SKU = Guid.NewGuid().ToString(), IsArchived = false };
-
-  service.AddProduct(newProduct);
-  return TypedResults.Created($"/products/{newProduct.Id}", newProduct);
-}).AddEndpointFilter<ProductValidationFilter>();
-
-app.MapPut("/products/{id}", (Product product, int id, IProductService service) =>
-{
-  return service.UpdateProduct(product, id);
-});
-
-app.MapDelete("/products/{id}", (int id, IProductService service) =>
-{
-  var product = service.DeleteProduct(id);
-});
+app.MapProductRoutes();
 
 app.Run();
