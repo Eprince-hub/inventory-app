e34f5586b7e871217d9cc30beff82f4bfdb74fb5
Victor Ejike Nwosu <ejikello123@yahoo.com>
2025-06-11 17:55:09 +0200

    Add ProductRoutes class to define API endpoints for product management

---
+++ InventoryApp.API/Routes/ProductRoutes.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using InventoryApp.Shared;
+
+public static class ProductRoutes
+{
+  public static IEndpointRouteBuilder MapProductRoutes(this IEndpointRouteBuilder app)
+  {
+    var productRouteGroup = app.MapGroup("/products");
+
+
+    productRouteGroup.MapGet("/", (IProductService service) => service.GetProducts());
+
+    productRouteGroup.MapGet("/{id}", Results<Ok<Product>, NotFound> (int id, IProductService service) =>
+    {
+      var product = service.GetProduct(id);
+      return product is not null ? TypedResults.Ok(product) : TypedResults.NotFound();
+    });
+
+    productRouteGroup.MapPost("/create", (Product product, IProductService service) =>
+    {
+      var newProduct = product with { Id = service.GetProducts().Count + 1, SKU = Guid.NewGuid().ToString(), IsArchived = false };
+
+      service.AddProduct(newProduct);
+      return TypedResults.Created($"/products/{newProduct.Id}", newProduct);
+    }).AddEndpointFilter<ProductValidationFilter>();
+
+    productRouteGroup.MapPut("/{id}", (Product product, int id, IProductService service) =>
+    {
+      return service.UpdateProduct(product, id);
+    });
+
+    productRouteGroup.MapDelete("/{id}", (int id, IProductService service) =>
+    {
+      var product = service.DeleteProduct(id);
+    });
+
+    return app;
+  }
+}
