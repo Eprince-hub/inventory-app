1cdcb52678f3c8a31c41a98083dd55b4fb206b5e
Victor Ejike Nwosu <ejikello123@yahoo.com>
2025-06-13 01:31:20 +0200

    Refactor Program.cs to configure InventoryDbContext for database operations and remove commented singleton service registration

--- InventoryApp.API/Program.cs
+++ InventoryApp.API/Program.cs
@@ -1,8 +1,14 @@
 // Similar to express server
+using InventoryApp.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Dependencies Injection
-builder.Services.AddSingleton<IProductService>(new ProductService());
+// builder.Services.AddSingleton<IProductService>(new ProductService());
+builder.Services.AddDbContext<InventoryDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("InventoryDatabase")));
+builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ProductValidationFilter>();
 
 // App Builder
